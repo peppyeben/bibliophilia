c8e24afd98d43426e5432c17bd8e9e9e96b78e57
peppyeben <austineben@gmail.com>
2024-03-12 10:21:12 +0100

    1

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+
+const routes = require("./routes/routes");
+const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+app.use("/api/v1/", routes);
+app.use(notFound);
+app.use(errorHandler);
+
+// connect db
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server listening at port ${port}`));
+    // app.listen();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
